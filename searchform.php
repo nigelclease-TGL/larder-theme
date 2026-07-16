1690311fd1a67d2b5f1a7445173f700f6f6911b8
nigelclease-TGL <nigelclease@gmail.com>
2026-07-16 17:59:23 +0200

    Add branded search form

---
+++ searchform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Search form.
+ *
+ * @package Larder
+ */
+?>
+<form role="search" method="get" class="larder-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'larder' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search recipes or ingredients…', 'placeholder', 'larder' ); ?>" value="<?php echo get_search_query(); ?>" name="s">
+	</label>
+	<button type="submit" class="button button-primary"><?php echo esc_html_x( 'Search', 'submit button', 'larder' ); ?></button>
+</form>
