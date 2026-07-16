c53bee6002f1561a93b6abb22a02f4e0c77bcf05
nigelclease-TGL <nigelclease@gmail.com>
2026-07-16 22:40:24 +0200

    Use editable social links on the Contact page

--- page-contact-me.php
+++ page-contact-me.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * Contact page template for the existing "contact-me" page.
+ * Contact page template for the contact-me and contact pages.
  *
  * @package Larder
  */
 
 get_header();
+$instagram_url = get_theme_mod( 'larder_instagram_url', 'https://www.instagram.com/thegourmetlarder/' );
+$pinterest_url = get_theme_mod( 'larder_pinterest_url', 'https://hu.pinterest.com/thegourmetlarder/' );
+$facebook_url  = get_theme_mod( 'larder_facebook_url', 'https://www.facebook.com/thegourmetlarder/' );
 ?>
 <main id="primary" class="contact-page">
 	<?php while ( have_posts() ) : the_post(); ?>
@@ -28,9 +31,9 @@ get_header();
 						<p class="eyebrow"><?php esc_html_e( 'Elsewhere', 'larder' ); ?></p>
 						<h2><?php esc_html_e( 'Follow the kitchen', 'larder' ); ?></h2>
 						<ul class="contact-page__socials">
-							<li><a href="https://www.instagram.com/thegourmetlarder/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-							<li><a href="https://hu.pinterest.com/thegourmetlarder/" target="_blank" rel="noopener noreferrer">Pinterest</a></li>
-							<li><a href="https://www.facebook.com/thegourmetlarder/" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+							<?php if ( $instagram_url ) : ?><li><a href="<?php echo esc_url( $instagram_url ); ?>" target="_blank" rel="noopener noreferrer">Instagram</a></li><?php endif; ?>
+							<?php if ( $pinterest_url ) : ?><li><a href="<?php echo esc_url( $pinterest_url ); ?>" target="_blank" rel="noopener noreferrer">Pinterest</a></li><?php endif; ?>
+							<?php if ( $facebook_url ) : ?><li><a href="<?php echo esc_url( $facebook_url ); ?>" target="_blank" rel="noopener noreferrer">Facebook</a></li><?php endif; ?>
 						</ul>
 					</div>
 				</aside>
@@ -38,4 +41,4 @@ get_header();
 		</section>
 	<?php endwhile; ?>
 </main>
-<?php get_footer();
+<?php get_footer(); ?>
