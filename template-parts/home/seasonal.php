8a1660febca71d3c349a8d71804a7a221a87f0ec
nigelclease-TGL <nigelclease@gmail.com>
2026-07-16 22:29:32 +0200

    Resolve seasonal collections from existing categories or pages

--- template-parts/home/seasonal.php
+++ template-parts/home/seasonal.php
@@ -5,12 +5,62 @@
  * @package Larder
  */
 
-$seasonal_categories = array(
-	'christmas' => __( 'Christmas baking', 'larder' ),
-	'easter'    => __( 'Easter favourites', 'larder' ),
-	'summer'    => __( 'Summer desserts', 'larder' ),
-	'autumn'    => __( 'Autumn comfort', 'larder' ),
+$seasonal_definitions = array(
+	array(
+		'label'          => __( 'Christmas baking', 'larder' ),
+		'class'          => 'christmas',
+		'category_slugs' => array( 'christmas' ),
+		'page_slugs'     => array( 'christmas' ),
+	),
+	array(
+		'label'          => __( 'Easter favourites', 'larder' ),
+		'class'          => 'easter',
+		'category_slugs' => array( 'easter', 'easter-baking' ),
+		'page_slugs'     => array( 'easter-baking', 'easter' ),
+	),
+	array(
+		'label'          => __( 'Spring & summer', 'larder' ),
+		'class'          => 'summer',
+		'category_slugs' => array( 'spring-summer', 'summer' ),
+		'page_slugs'     => array( 'spring-summer', 'summer' ),
+	),
+	array(
+		'label'          => __( 'Autumn & winter', 'larder' ),
+		'class'          => 'autumn',
+		'category_slugs' => array( 'autumn-winter', 'autumn' ),
+		'page_slugs'     => array( 'autumn-winter', 'autumn' ),
+	),
 );
+
+$seasonal_links = array();
+
+foreach ( $seasonal_definitions as $definition ) {
+	$url = '';
+
+	foreach ( $definition['category_slugs'] as $category_slug ) {
+		$category = get_category_by_slug( $category_slug );
+		if ( $category ) {
+			$url = get_category_link( $category->term_id );
+			break;
+		}
+	}
+
+	if ( ! $url ) {
+		$page = nkt_setup_find_page( $definition['page_slugs'] );
+		if ( $page ) {
+			$url = get_permalink( $page );
+		}
+	}
+
+	if ( $url ) {
+		$definition['url'] = $url;
+		$seasonal_links[]  = $definition;
+	}
+}
+
+if ( empty( $seasonal_links ) ) {
+	return;
+}
 ?>
 <section class="home-section seasonal-collections" aria-labelledby="seasonal-title">
 	<div class="container">
@@ -22,12 +72,9 @@ $seasonal_categories = array(
 		</header>
 
 		<div class="seasonal-grid">
-			<?php foreach ( $seasonal_categories as $slug => $label ) :
-				$category = get_category_by_slug( $slug );
-				$url      = $category ? get_category_link( $category ) : home_url( '/category/' . $slug . '/' );
-			?>
-				<a class="seasonal-card seasonal-card--<?php echo esc_attr( $slug ); ?>" href="<?php echo esc_url( $url ); ?>">
-					<span class="seasonal-card__label"><?php echo esc_html( $label ); ?></span>
+			<?php foreach ( $seasonal_links as $collection ) : ?>
+				<a class="seasonal-card seasonal-card--<?php echo esc_attr( $collection['class'] ); ?>" href="<?php echo esc_url( $collection['url'] ); ?>">
+					<span class="seasonal-card__label"><?php echo esc_html( $collection['label'] ); ?></span>
 					<span class="seasonal-card__link"><?php esc_html_e( 'Explore recipes', 'larder' ); ?> →</span>
 				</a>
 			<?php endforeach; ?>
