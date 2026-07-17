d26cd4f7e94cf15ae4eeb160ca29d1d5bbdb23a2
nigelclease-TGL <nigelclease@gmail.com>
2026-07-17 23:24:45 +0200

    Load final homepage polish stylesheet

--- functions.php
+++ functions.php
@@ -94,6 +94,7 @@ function larder_enqueue_assets() {
 	wp_enqueue_style( 'nkt-brand-v2', get_template_directory_uri() . '/assets/css/brand-v2.css', array( 'nkt-brand-system' ), $version );
 	wp_enqueue_style( 'nkt-home-phase-2', get_template_directory_uri() . '/assets/css/home-phase-2.css', array( 'nkt-brand-v2' ), $version );
 	wp_enqueue_style( 'nkt-home-phase-2-finish', get_template_directory_uri() . '/assets/css/home-phase-2-finish.css', array( 'nkt-home-phase-2' ), $version );
+	wp_enqueue_style( 'nkt-home-phase-2-final', get_template_directory_uri() . '/assets/css/home-phase-2-final.css', array( 'nkt-home-phase-2-finish' ), $version );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
