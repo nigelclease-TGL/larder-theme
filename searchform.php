7cc913cf288fd474c0385b6091acdb75cb697838
nigelclease-TGL <nigelclease@gmail.com>
2026-07-18 06:11:01 +0200

    Refine the global recipe search form

--- searchform.php
+++ searchform.php
@@ -5,10 +5,10 @@
  * @package Larder
  */
 ?>
-<form role="search" method="get" class="larder-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="search-form larder-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'larder' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search recipes or ingredients…', 'placeholder', 'larder' ); ?>" value="<?php echo get_search_query(); ?>" name="s">
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search recipes or ingredients…', 'placeholder', 'larder' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" autocomplete="off">
 	</label>
-	<button type="submit" class="button button-primary"><?php echo esc_html_x( 'Search', 'submit button', 'larder' ); ?></button>
+	<button type="submit" class="search-submit button button-primary"><?php echo esc_html_x( 'Search', 'submit button', 'larder' ); ?></button>
 </form>
